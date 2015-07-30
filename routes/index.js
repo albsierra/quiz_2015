aba02c700f99fa3ee18673c4cc4dad6924e98756
Alberto Sierra <alberto.sierra@murciaeduca.es>
2015-07-30 18:20:59 +0200

    Lista de preguntas y Autoload

--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,12 @@ router.get('/author', function(req, res) {
   res.render('author', { author: 'Alberto Sierra Olmo' });
 });
 
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer', quizController.answer);
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load);
+
+//Definición de rutas de /quizes
+router.get('/quizes', quizController.index);
+router.get('/quizes/:quizId(\\d+)', quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
 module.exports = router;
