21d3a0744ae4d2b8646ae06a97261ed20b5f1b63
Alberto Sierra <alberto.sierra@murciaeduca.es>
2015-08-02 20:40:26 +0200

    Crear preguntas y validación

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -2,7 +2,7 @@ var models = require('../models/models.js');
 
 // Autoload - factoriza el código si ruta incluye :quizId
 exports.load = function(req, res, next, quizId) {
-	models.Quiz.find(quizId).then(
+	models.Quiz.findById(quizId).then(
 		function(quiz) {
 			if (quiz) {
 				req.quiz = quiz;
@@ -21,14 +21,14 @@ exports.index = function(req,res,next) {
 
 	models.Quiz.findAll({where:["pregunta like ?", '%'+filtro+'%'],order:'pregunta ASC'}).then(
 		function(quizes) {
-			res.render('quizes/index', {quizes: quizes});
+			res.render('quizes/index', {quizes: quizes, errors: [] });
 		}
 	).catch(function(error) {next(error);})
 };
 
 // GET /quizes/:id
 exports.show = function(req,res) {
-		res.render('quizes/show', {quiz: req.quiz});
+		res.render('quizes/show', {quiz: req.quiz, errors: [] });
 };
 
 // GET /quizes/:id/answer
@@ -37,5 +37,30 @@ exports.answer = function(req,res) {
 	if (req.query.respuesta === req.quiz.respuesta){
 		resultado = 'Correcto';
 	}
-	res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
+	res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado, errors: [] });
+};
+
+// GET /quizes/new
+exports.new = function(req,res) {
+	var quiz = models.Quiz.build( //crea objeto quiz
+		{pregunta: "Pregunta", respuesta: "Respuesta"}
+	);
+
+	res.render('quizes/new', {quiz: quiz, errors: [] });
+};
+
+// POST /quizes/create
+exports.create = function(req,res) {
+	var quiz = models.Quiz.build( req.body.quiz );
+
+	quiz.validate().then(function(err) {
+		if(err){
+			res.render('quizes/new', {quiz: quiz, errors: err.errors});
+		}else{
+			//guarda en DB los campos pregunta y respuesta de quiz
+			quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+				res.redirect('/quizes');
+			}) // Redirección HTTP (URL relativo) lista de preguntas
+		}
+	});
 };
--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,12 @@ var quizController = require('../controllers/quiz_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz' });
+  res.render('index', { title: 'Quiz', errors: []  });
 });
 
 /* GET créditos. */
 router.get('/author', function(req, res) {
-  res.render('author', { author: 'Alberto Sierra Olmo' });
+  res.render('author', { author: 'Alberto Sierra Olmo', errors: []  });
 });
 
 // Autoload de comandos con :quizId
@@ -20,5 +20,7 @@ router.param('quizId', quizController.load);
 router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+router.get('/quizes/new', quizController.new);
+router.post('/quizes/create', quizController.create);
 
 module.exports = router;
